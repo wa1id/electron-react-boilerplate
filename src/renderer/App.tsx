2918f42a8f525777313d66a5684f9d37324b6c76
Walid Yachou <walid.yachou@capgemini.com>
2021-09-03 12:33:21 +0200

    feature: add tailwind

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -9,7 +9,7 @@ const Hello = () => {
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1 className="text-black">electron-react-boilerplate</h1>
       <div className="Hello">
         <a
           href="https://electron-react-boilerplate.js.org/"
