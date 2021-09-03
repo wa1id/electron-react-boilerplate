54c6c4639e9af525f0b39414bd390d55d7fd5d1b
Walid Yachou <walid.yachou@capgemini.com>
2021-09-03 13:45:31 +0200

    feature: working firebase

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 import Button from '@material-ui/core/Button';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
+import app from './firebaseapp';
 
 const Hello = () => {
+  const db = getFirestore(app);
+
+  const myfunc = async () => {
+    await setDoc(doc(db, 'cities', 'LA'), {
+      name: 'Los Angeles',
+      state: 'CA',
+      country: 'USA',
+    });
+  };
+
   return (
     <div>
       <h1>electron-react-boilerplate</h1>
       <h2>electron-react-boilerplate</h2>
       <h3>electron-react-boilerplate</h3>
       <h4>electron-react-boilerplate</h4>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color="primary" onClick={() => myfunc()}>
         Test
       </Button>
     </div>
---
+++ src/renderer/firebaseapp.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAf8NYi8QoxS6ElmEJTZaEXsPe7bnje1sA',
+  authDomain: 'test-electron-9886b.firebaseapp.com',
+  projectId: 'test-electron-9886b',
+  storageBucket: 'test-electron-9886b.appspot.com',
+  messagingSenderId: '733084442207',
+  appId: '1:733084442207:web:3e0350cc48d78ce042c5ea',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
